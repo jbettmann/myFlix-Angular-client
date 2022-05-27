78fbfe8d2f3cde26e5089aafff3501dd339bfc0c
jbettmann <jordan.bettmann@gmail.com>
2022-05-27 15:04:14 -0600

    removed welcome components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component } from '@angular/core';
-import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
-import { MatDialog } from '@angular/material/dialog';
-import { UserLoginComponent } from './user-login/user-login.component';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,21 +7,4 @@ import { UserLoginComponent } from './user-login/user-login.component';
 })
 export class AppComponent {
   title = 'myFlix-Angular-client';
-
-  // pass Angular Material dialog in constructor as and argument for use in component
-  constructor(public dialog: MatDialog) {}
-  // This is the function that will open the dialog when the signup button is clicked
-  openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-      // Assigning the dialog a width
-      width: '280px',
-    });
-  }
-
-  openUserLoginDialog(): void {
-    this.dialog.open(UserLoginComponent, {
-      // Assigning the dialog a width
-      width: '280px',
-    });
-  }
 }
