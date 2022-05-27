636ddc80f8bbe267858d4273a84246386ac35d6c
jbettmann <jordan.bettmann@gmail.com>
2022-05-27 15:04:30 -0600

    added all views and routes needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,18 +15,47 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { FormsModule } from '@angular/forms';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { MovieCardComponent } from './movie-card/movie-card.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+
+// Angular's built-in Routing
+import { RouterModule, Routes } from '@angular/router';
+
+import { MatIconModule } from '@angular/material/icon';
+import { DirectorViewComponent } from './director-view/director-view.component';
+import { GenreViewComponent } from './genre-view/genre-view.component';
+import { SynopsisViewComponent } from './synopsis-view/synopsis-view.component';
+import { ProfileComponent } from './profile/profile.component';
+import { UserUpdateDialogComponent } from './user-update-dialog/user-update-dialog.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  { path: 'movies', component: MovieCardComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: '', redirectTo: 'welcome', pathMatch: 'prefix' },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     UserRegistrationFormComponent,
     UserLoginComponent,
+    MovieCardComponent,
+    WelcomePageComponent,
+    DirectorViewComponent,
+    GenreViewComponent,
+    SynopsisViewComponent,
+    ProfileComponent,
+    UserUpdateDialogComponent,
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    MatIconModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
@@ -38,6 +67,7 @@ import { UserLoginComponent } from './user-login/user-login.component';
     MatDialogModule,
     MatSnackBarModule,
     FormsModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
